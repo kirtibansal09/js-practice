3905bad2e58660f0c06681842ad0134a3301a6c4
kirtibansal09 <kirtibansalbikaner@gmail.com>
2024-04-21 12:57:18 +0530

    variables and datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"  //treat all js code as newer version
+// alert(3+3)  // we are using nodejs not browser
+
+
+//readability imp
+console.log(3+3);
+
+console.log("Hitesh");
+
+//original documentation of js: tc.39es  ecma script 
+//ecma script write standards 
+//mdn 
+
+let name = "kirti"  //string
+let age = 18        //number
+let isLoggedIn = false   //boolean
+
+
+/** Primitive */
+// null => standalone value
+// undefined => 
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//symbol => unique 
+
+
+/** Object */
+
+console.log(typeof null); //object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =14553
+let accountEmail = "kirtibansalgmail.com" // space reserved in memory for these variables
+var accountPassword = "12345"
+accounyCity = "Jaipur"
+let accountState;
+
+// let and var dynamic changeable
+// const not changeable
+
+// accountId = 2  //not allowed
+
+//var has the problem of scope, therefore it is avoided
+
+accountEmail = "efewfe"
+accountPassword="212w1221"
+accounyCity ="bengaluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId, accountPassword, accounyCity, accountState])
