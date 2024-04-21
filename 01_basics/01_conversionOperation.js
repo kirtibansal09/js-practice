298c58f3aaec3144b9ef4823a9df35798b0d8886
kirtibansal09 <kirtibansalbikaner@gmail.com>
2024-04-21 13:21:58 +0530

    conversion confusion

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score)); //can write it as a method in parathesis
+// console.log(typeof score);
+// console.log(typeof (score)); //can write it as a method in parathesis
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 let booleanLOggedIn = Boolean(isLoggedIn)
-console.log(booleanLOggedIn);
+// console.log(booleanLOggedIn);
 
 // 1=> true; 0 => false
 // " " = false
@@ -21,5 +21,23 @@ console.log(booleanLOggedIn);
 
 let someNumber = 33
 let staringNumber  = String(someNumber)
-console.log(staringNumber);
-console.log(typeof staringNumber);
+// console.log(staringNumber);
+// console.log(typeof staringNumber);
+
+
+// ******* Operations **********
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " kits"
+
+let str3 = str1+ str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" +2 + 2);
+console.log(1 +2+"2");
